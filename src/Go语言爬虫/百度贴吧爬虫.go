51fa2fe0d5df613a9f914c264fe12253b5e2a905
Mrwangkedong <www.1241925557@qq.com>
2021-01-22 16:50:18 +0800

    golang爬虫

--- src/Go语言爬虫/百度贴吧爬虫.go
+++ src/Go语言爬虫/百度贴吧爬虫.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 	"strconv"
 )
 
@@ -26,17 +28,51 @@ func main() {
 
 func Spider(start int, end int) {
 	fmt.Printf("正在爬取第%d页到%d页....\n", start, end)
-
-	for i := start; i < end; i++ {
+	//爬取每一页的数据
+	for i := start; i <= end; i++ {
 		url := "https://tieba.baidu.com/f?kw=%E9%83%91%E7%88%BD&ie=utf-8&pn=" + strconv.Itoa((i-1)*50)
-		httpGet(url)
+		result, err := httpGet(url)
+		if err != nil {
+			fmt.Println("获取http内容错误：err ", err)
+		}
+		//fmt.Println(result)
+		//将保存的网页内容存到一个文件中去
+		f, err := os.Create("G:\\GolangFileText\\爬虫第" + strconv.Itoa(i) + "页.html")
+		if err != nil {
+			fmt.Println("os.Create：err ", err)
+		}
+		_, _ = f.Write([]byte(result))
+		fmt.Printf("第%d页成功\n", i)
+		defer f.Close() //保存好一个文件，关闭一个文件
 	}
 }
 
-func httpGet(url string) (result string) {
-	response, err := http.Get(url)
-	if err != nil {
-		fmt.Println("http.Get(url) err: ", err)
+func httpGet(url string) (result string, err error) {
+	response, err1 := http.Get(url)
+	if err1 != nil {
+		err = err1
+		return
 	}
+
+	defer response.Body.Close()
+
+	//循环读取网页数据，传出给调用者
+	buf := make([]byte, 4096)
+	result = ""
+	for true {
+		n, err2 := response.Body.Read(buf)
+		if n == 0 {
+			fmt.Println("读取网页完成")
+			break
+		}
+		if err2 != nil && err2 != io.EOF {
+			err = err2
+			return
+		}
+		//累加每一次循环
+		result += string(buf[:n])
+	}
+
+	return result, err
 
 }
