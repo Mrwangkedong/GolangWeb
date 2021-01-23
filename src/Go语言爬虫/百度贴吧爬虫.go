8602af6d08d570ebfe336493b4a4b08109749d39
Mrwangkedong <www.1241925557@qq.com>
2021-01-23 11:23:14 +0800

    golang爬虫

--- src/Go语言爬虫/百度贴吧爬虫.go
+++ src/Go语言爬虫/百度贴吧爬虫.go
@@ -15,6 +15,15 @@ https://tieba.baidu.com/f?kw=%E9%83%91%E7%88%BD&ie=utf-8&pn=50
 https://tieba.baidu.com/f?kw=%E9%83%91%E7%88%BD&ie=utf-8&pn=100
 */
 
+/*
+1.提示用户指定爬的页数
+2.使用start，end爬取每一页
+3.获取每一页url ———— 下一页 = 前一页+50
+4.封装实现HttpGet()，爬取一个网页的数据内容，通过result返回
+5.创建 .html文件。使用循环因子i命名。
+6.将result写入文件，使用f.close()关闭，不推荐使用defer
+*/
+
 func main() {
 	//指定爬取的起始页、终止页
 	var start, end int
@@ -26,9 +35,11 @@ func main() {
 	Spider(start, end)
 }
 
+//爬取单个页面的函数
+
+//进行爬取
 func Spider(start int, end int) {
 	fmt.Printf("正在爬取第%d页到%d页....\n", start, end)
-	//爬取每一页的数据
 	for i := start; i <= end; i++ {
 		url := "https://tieba.baidu.com/f?kw=%E9%83%91%E7%88%BD&ie=utf-8&pn=" + strconv.Itoa((i-1)*50)
 		result, err := httpGet(url)
@@ -43,10 +54,11 @@ func Spider(start int, end int) {
 		}
 		_, _ = f.Write([]byte(result))
 		fmt.Printf("第%d页成功\n", i)
-		defer f.Close() //保存好一个文件，关闭一个文件
+		f.Close() //保存好一个文件，关闭一个文件
 	}
 }
 
+//获得网页源码
 func httpGet(url string) (result string, err error) {
 	response, err1 := http.Get(url)
 	if err1 != nil {
