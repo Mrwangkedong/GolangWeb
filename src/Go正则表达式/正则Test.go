b9edd68fc0d00db69c550648d23bbbd3a8f40a9e
Mrwangkedong <www.1241925557@qq.com>
2021-01-24 17:08:36 +0800

    正则

--- src/Go正则表达式/正则Test.go
+++ src/Go正则表达式/正则Test.go
@@ -25,32 +25,21 @@ func main() {
 	fmt.Println(strList)
 
 	//go语言定义多行字符串，利用 ` `
-	str = `<html lang="zh-CN">
-	<head>
-	<title>Go语言标准库文档中文版 | Go语言中文网 | Golang中文社区 | Golang中国</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
-	<meta charset="utf-8">
-	<link rel="shortcut icon" href="/static/img/go.ico">
-	<link rel="apple-touch-icon" type="image/png" href="/static/img/logo2.png">
-	<meta name="author" content="polaris <polaris@studygolang.com>">
-	<meta name="keywords" content="中文, 文档, 标准库, Go语言,Golang,Go社区,Go中文社区,Golang中文社区,Go语言社区,Go语言学习,学习Go语言,Go语言学习园地,Golang 中国,Golang中国,Golang China, Go语言论坛, Go语言中文网">
-	<meta name="description" content="Go语言文档中文版，Go语言中文网，中国 Golang 社区，Go语言学习园地，致力于构建完善的 Golang 中文社区，Go语言爱好者的学习家园。分享 Go 语言知识，交流使用经验">
-	</head>
-	<frameset cols="15,85">
-	<frame src="/static
-/pkgdoc/i.html">
-	<frame name="main" src="/static/pkgdoc/main.html" tppabs="main.html" >
-	<noframes>
-	</noframes>
-	</frameset>
-	</html>`
+	str = `<div class="hd">
+                        <a href="https://movie.douban.com/subject/1308857/" class="">
+                            <span class="title">可可西里</span>
+                                <span class="other">&nbsp;/&nbsp;Kekexili: Mountain Patrol</span>
+                        </a>`
 	//提取标签<frame>标签中的src
 	//1.解析编译正则表达式
-	re = regexp.MustCompile(`<frame.*src="(?s:(.*?))"`)
+	re = regexp.MustCompile(`<div class="hd">.*\n.*\n.*<span class="title">(?s:(.*?))</span>`)
 	//2.获得结果
 	strList = re.FindAllStringSubmatch(str, -1)
 	fmt.Println(strList)
+
+	//for _,item := range strList{
+	//	fmt.Printf("item[0]: %s\titem[1]: %s\n",item[0],item[1])
+	//}
 
 	/*输出
 	[[<frame src="/static
